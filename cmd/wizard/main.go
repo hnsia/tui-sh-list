bc6661859eb4fe0f64aa4b42378237565a079934
hnsia <44498178+hnsia@users.noreply.github.com>
2024-12-20 20:27:39 +0800

    feat: implement default stylings on input field

--- cmd/wizard/main.go
+++ cmd/wizard/main.go
@@ -8,17 +8,37 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+
+	return s
+}
+
 type model struct {
 	questions   []string
 	width       int
 	height      int
 	index       int
 	answerField textinput.Model
+	styles      *Styles
 }
 
 func New(questions []string) *model {
+	styles := DefaultStyles()
 	answerField := textinput.New()
-	return &model{questions: questions, answerField: answerField}
+	answerField.Placeholder = "Your answer here"
+	return &model{
+		questions:   questions,
+		answerField: answerField,
+		styles:      styles,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -43,11 +63,19 @@ func (m model) View() string {
 	if m.width == 0 {
 		return "loading..."
 	}
-	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
+	return lipgloss.JoinVertical(
+		lipgloss.Center,
+		m.questions[m.index],
+		m.styles.InputField.Render(m.answerField.View()),
+	)
 }
 
 func main() {
-	questions := []string{"what is your name?", "what is your favourite editor?", "what is your favourite quote?"}
+	questions := []string{
+		"what is your name?",
+		"what is your favourite editor?",
+		"what is your favourite quote?",
+	}
 	m := New(questions)
 
 	f, err := tea.LogToFile("debug.log", "debug")
